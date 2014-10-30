d7682c854cc521baf0b4a74d0e0278a402e88f42
jquigley1003 <jeff.quigley@wavinghi.com>
2014-10-30 19:36:59 -0400

    user controller updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController <ApplicationController
+  before_action :signed_in_user, only: [:show]
+  before_action :correct_user, only: [:edit, :update, :destroy] 
 
   def new
     @user = User.new
