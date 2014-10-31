1dfdc9c23cdde9a18ff28fb36309dbc500900b2b
jquigley1003 <jeff.quigley@wavinghi.com>
2014-10-31 00:47:58 -0400

    make it pretty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController <ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       redirect_to search_path, notice: "Welcome!"
     else
       render 'new'
